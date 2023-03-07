aad311accb440a23180dd4930c27520de1ecffb5
DigiLeaf <103275687+DigiLeaf@users.noreply.github.com>
2023-03-07 12:54:33 -0600

    Added program description

--- BMImain.cpp
+++ BMImain.cpp
@@ -1,6 +1,15 @@
 #include "BMI functions.h"
 #include <iostream>
 
+/*	Program name: BMI calculator
+	Author: Leif Orth
+	NetId: leo94
+	The goal of this program is to be a BMI calculator by taking
+	in the user's height and weight then calculating their BMI and
+	returning what BMI range they fall into.
+	*/
+
+
 using namespace std;
 
 
